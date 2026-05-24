59e54a74ef76e3bb2698532edce894dcb2ce7387
Abhi03onGitHub <er.abhinav.3@gmail.com>
2026-05-24 15:25:04 +0530

    Adding 12th code

---
+++ src/main/java/org/example/bubbleSort.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class bubbleSort {
+    public static void main (String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        System.out.print("Bubble Sort = ");
+        for(int num : arr)
+            System.out.print(num +" ");
+    }
+}
+
