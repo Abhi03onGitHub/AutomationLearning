a7236f98beb0d30b67806bda75848c3be60cd4d3
Abhi03onGitHub <er.abhinav.3@gmail.com>
2026-05-24 15:30:09 +0530

    Adding 14th code

---
+++ src/main/java/org/example/PrimeNumber.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class PrimeNumber {
+    public static void main (String[] args) {
+        int num = 13;
+
+        boolean isPrime = true;
+
+        for (int i = 2; i <= num / 2; i++) {
+
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(num + " is Prime");
+        } else {
+            System.out.println(num + " is Not Prime");
+        }
+    }
+}
